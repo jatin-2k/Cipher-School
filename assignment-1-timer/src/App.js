4f6260e04e5b4ca92c4f99cdd694e5753ea31235
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-01-27 14:23:58 +0530

    timer done

--- assignment-1-timer/src/App.js
+++ assignment-1-timer/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startTime: new Date().getTime(),
+      curTime: 0,
+      isRunning: false,
+      isPause: false
+    };
+  }
+  componentDidMount() {
+    this.tick();
+    setInterval(this.tick.bind(this), 10);
+  }
+
+  //#region Methods
+  tick() {
+    if(this.state.isRunning && !this.state.isPause){
+      this.setState({
+        curTime: new Date().getTime() - this.state.startTime
+      });
+    }
+    else if(!this.state.isRunning){
+      this.setState({
+        curTime: 0,
+      });
+    }
+  }
+  start(){
+    this.setState({
+      startTime: new Date().getTime(),
+      isRunning: true,
+      isPause: false
+    })
+  }
+  playpause(){
+
+    if(this.state.isPause){
+      this.setState({
+        startTime: new Date().getTime()-this.state.curTime
+      });
+    }
+
+    this.setState({
+      isPause: !this.state.isPause
+    });
+  }
+  reset(){
+    this.setState({
+      isRunning: false,
+      isPause: false,
+      curTime: 0,
+    })
+  }
+  //#endregion
+
+  render() {
+    return (
+      <div className="App">
+        <h2>{ 
+          parseInt(this.state.curTime/1000/60/60)+":"+
+          parseInt(this.state.curTime/1000/60)%60+":"+
+          parseInt(this.state.curTime/1000)%60+":"
+          +this.state.curTime%1000}
+        </h2>
+        <button onClick = {this.start.bind(this)}>Start</button>
+        <button onClick = {this.playpause.bind(this)}>{this.state.isPause? "Resume": "Pause"}</button>
+        <button onClick = {this.reset.bind(this)}>Reset</button>
+      </div>
+    );
+  }
 }
-
 export default App;
